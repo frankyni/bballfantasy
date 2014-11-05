a69cccfc7d93c302e18032200afd8760d3ac89ae
frankyni <franky.chenxni@gmail.com>
2014-11-05 15:59:08 -0500

    why cant you more chill yahoo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,8 @@
 class WelcomeController < ApplicationController
 	def index
 	 # # cf http://oauth.rubyforge.org/rdoc/classes/OAuth/AccessToken.html
-		if current_user?
-			@consumer_tokens=YahooToken.all :conditions=>{:user_id=>current_user.id}
-			@token = @consumer_tokens.first.client
-			logger.info "private data: "+@token.get("/data/index").body
-		end
+	# @consumer_tokens=YahooToken.all :conditions=>{:user_id=>current_user.id}
+	# @token = @consumer_tokens.first.client
+	# logger.info "private data: "+@token.get("/data/index").body
 	end
 end
