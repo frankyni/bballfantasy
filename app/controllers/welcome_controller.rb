2266525d3db1ad542258c5c73eabcbb0df24609d
frankyni <franky.chenxni@gmail.com>
2014-11-05 15:43:26 -0500

    deivse not working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+	def index
+	 # cf http://oauth.rubyforge.org/rdoc/classes/OAuth/AccessToken.html
+	 @consumer_tokens=YahooToken.all :conditions=>{:user_id=>current_user.id}
+	 @token = @consumer_tokens.first.client
+	 logger.info "private data: "+@token.get("/data/index").body
+	end
 end
