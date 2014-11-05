fbbd01f13f0d7b7feee4232463013b762ec80aae
frankyni <franky.chenxni@gmail.com>
2014-11-05 15:54:50 -0500

    testing more stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
 	def index
 	 # # cf http://oauth.rubyforge.org/rdoc/classes/OAuth/AccessToken.html
-	 # @consumer_tokens=YahooToken.all :conditions=>{:user_id=>current_user.id}
-	 # @token = @consumer_tokens.first.client
-	 # logger.info "private data: "+@token.get("/data/index").body
+		if current_user? do
+			@consumer_tokens=YahooToken.all :conditions=>{:user_id=>current_user.id}
+			@token = @consumer_tokens.first.client
+			logger.info "private data: "+@token.get("/data/index").body
+		end
 	end
 end
