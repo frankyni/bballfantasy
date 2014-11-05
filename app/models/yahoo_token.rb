094aea4bd67f95cd047c18cbffe31b2d5ffa13af
frankyni <franky.chenxni@gmail.com>
2014-11-05 16:28:59 -0500

    yahoo being a bitch

--- app/models/yahoo_token.rb
+++ app/models/yahoo_token.rb
@@ -1,9 +1,9 @@
 class YahooToken < ConsumerToken
 	YAHOO_SETTINGS = {
-		:site => "https://morning-woodland-3491.herokuapp.com",
-		:request_token_path => "/oauth/request_token",
-		:access_token_path => "/oauth/access_token",
-		:authorize_path => "/oauth/authorize"
+    	:site=>"http://fantasysports.yahooapis.com/fantasy/v2",
+    	:authorize_url =>"https://api.login.yahoo.com/oauth/v2/request_auth",
+    	:request_token_url => "https://api.login.yahoo.com/oauth/v2/get_request_token",
+    	:access_token_url => "https://api.login.yahoo.com/oauth/v2/get_token",
 	}
 
 	def self.consumer(options={})
